cc32b633ebadbe5dcbe9173fbb4edab990adcd2d
Calvin Cheung <cheungcalvin2@gmail.com>
2020-11-02 12:43:59 -0500

    Revert "Create basic UI for currency conversion"
    This reverts commit 259ad4378a7eddac19e98f6c81cea98905cd035f.

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <currency-converter/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-import CurrencyConverter from './components/CurrencyConverter.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
-    'currency-converter': CurrencyConverter
+    HelloWorld
   }
 }
 </script>
