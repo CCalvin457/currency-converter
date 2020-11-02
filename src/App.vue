4e3235a9018d9354f65a94dc73643c4324d3ecb2
Calvin Cheung <cheungcalvin2@gmail.com>
2020-11-02 13:05:09 -0500

    Including the currency converter component to app.js to view basic UI created

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <currency-converter/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CurrencyConverter from './components/CurrencyConverter.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'currency-converter': CurrencyConverter
   }
 }
 </script>
